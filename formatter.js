c317a121a2a0b460b3bd158954a5b87b579cc391
姜涛 <jiangtao3@mafengwo.com>
2022-04-08 16:53:02 +0800

    优化标点符号替换

--- formatter.js
+++ formatter.js
@@ -1,10 +1,6 @@
 function format(str) {
-    let a = str.replace(/,/g, "\n\n");
-    let b = a.replace(/，/g, "\n\n");
-    let c = b.replace(/。/g, "\n\n");
-    let d = c.replace(/"/g, "\n\n");
-    let e = d.replace(/“/g, "\n\n");
-    let f = e.replace(/”/g, "\n\n");
+    let regex = /,|，|。|"|“|”|;|；|……/g;
+    let f = str.replace(regex, "\n\n");
     let strSize = calculate_byte(f);
     let timeStr = getCurrentTime();
     let location = "云岩区";
@@ -16,13 +12,11 @@ function calculate_byte(str) {
     var byteLen = 0;
     var cnCharByteLen = 1;
     for (var i = 0; i < str.length; i++) {
-        //alert（str.charAt（i））;
         if ((/[\x00-\xff]/g).test(str.charAt(i)))
             byteLen += 1;
         else
             byteLen += cnCharByteLen;
     }
-    //alert(byteLen);
     return byteLen;
 }
 
@@ -33,6 +27,6 @@ function getCurrentTime() {
     var dat = date.getDate(); //获取当前日
     var hour = date.getHours(); //获取小时
     var minutes = date.getMinutes(); //获取分钟
-    var timer = year + '年' + month + '月' + dat + '日' + ' ' + hour + ':' + minutes;
+    var timer = year + '年' + month + '月' + dat + '日' + hour + ':' + minutes;
     return timer;
 }
